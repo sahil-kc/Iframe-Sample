ea36470488b6bfd4cfc00b77a604404ef39c91b1
sahilkhurana19 <sahil_khurana@outlook.com>
2017-06-22 18:30:40 +0530

    Added js for batches + css fixes throughout the menu

--- js/app.js
+++ js/app.js
@@ -1,34 +1,54 @@
 console.log(document.body.clientHeight);
 console.log(screen.availHeight);
-document.getElementById("wasd").style.height = (screen.availHeight - 0.3 * screen.availHeight) + "px";
-document.getElementById("iframe").style.height = (screen.availHeight - 0.3 * screen.availHeight) + "px";
+if(window.screen.width > 768) {
+	document.getElementById("wasd").style.height = (screen.availHeight - 0.3 * screen.availHeight) + "px";
+	document.getElementById("iframe").style.height = (screen.availHeight - 0.3 * screen.availHeight) + "px";
+}
 $("document").ready(function(){
 	$(".year-select").select2({
 		placeholder: "Batch",
 		allowClear: true
+
 	})
+		
 })
 
-var iframeShifter = function(index) {
-	el = document.getElementById("iframe");
-	$("#subgroup1,#subgroup2,#subgroup3,#subgroup4").css({"background":"white", "color": "black"});
-	switch(index) {
-		case 0: el.src = "http://localhost:8000/kcgroups/kc-team/3?thirdparty=hatch&type=both";
+$(".getwhite").on("click", function(){
+	$("li.getgreen").removeClass("getgreen");
+	$(".select2-selection--single").attr({
+				id: ""
+			});
+	$(this).addClass("getgreen");
+	el = document.getElementById("iframe");	
+	
+	switch(parseInt(this.id.replace(/subgroup/, ""))) {
+		case 1: el.src = "http://localhost:8000/kcgroups/kc-team/3?thirdparty=hatch&type=both";
+			break;
+		case 2: el.src = "http://localhost:8000/kcgroups/karmasphere/1?thirdparty=hatch&type=both";
+			break;
+		case 3: el.src = "http://localhost:8000/kcgroups/logic-simplified/2?thirdparty=hatch&type=both";
+			break;
+		case 4 : 
+			$(".select2-selection--single").attr({
+				id: "greenid"
+			});
 
-			document.getElementById("subgroup1").style.background = "#3ebb95";
-			document.getElementById("subgroup1").style.color = "#fff";
+			$("select").on("select2:select", function(){
+				var array = [];
+				array.push(parseInt(newString = ($(".select2-selection__rendered").text().replace('×',''))))
+				switch(array[0]) {
+					case 1960: el.src = "http://localhost:8000/kcgroups/kc-team/3?thirdparty=hatch&type=both";
+						break;
+					case 1965: el.src = "http://localhost:8000/kcgroups/karmasphere/1?thirdparty=hatch&type=both";
+						break;
+					case 1970:el.src = "http://localhost:8000/kcgroups/logic-simplified/2?thirdparty=hatch&type=both";
+						break;
+					default: el.src = "http://localhost:8000/kcgroups/hatch-international/7?thirdparty=hatch&type=both";
+				}
+			})
+			
+			
 			break;
-		case 1: el.src = "http://localhost:8000/kcgroups/karmasphere/1?thirdparty=hatch&type=both";
-			document.getElementById("subgroup2").style.background = "#3ebb95";
-			document.getElementById("subgroup2").style.color = "#fff";
-			break;
-		case 2: el.src = "http://localhost:8000/kcgroups/logic-simplified/2?thirdparty=hatch&type=both";
-			document.getElementById("subgroup3").style.background = "#3ebb95";
-			document.getElementById("subgroup3").style.color = "#fff";
-			break;
-		case 3: el.src = "http://localhost:8000/kcgroups/hatch-international/7?thirdparty=hatch&type=both";
-			document.getElementById("subgroup4").style.background = "#3ebb95";
-			document.getElementById("subgroup4").style.color = "#fff";
-			break;
-	}
-}
+		default: console.log("index not found");
+		}
+})
